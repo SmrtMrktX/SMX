e75acfe068b5370583ecf7e208b2d7e61e7a393f
d3j1x <d3j1x@yahoo.com>
2023-12-27 22:06:36 +0100

    Start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { Blockquote } from "flowbite-svelte";
+
+
+
+  
+</script>
+
+<svelte:head>
+<title>SMRT MRKT X</title>
+<meta name="description" content="SMX | Smart Market X - Resources - Hints - Tips - Opensource ..." />
+</svelte:head>
+
+
+<div class="flex justify-center items-center p-8">
+ 
+<Blockquote alignment="center" size="xl">
+  
+  <span>"SMRT MRKT X Is Just Awesome Opensource.
+    <br>Best Of Smart Tips Hint Source..."</span>
+</Blockquote>
+</div>
+
+
+
+<div class="flex my-10 mx-10 justify-center items-center">
+
+  <h1 class="h-1/2">Welcome to our virtual home!
+    <br> We're thrilled to have you here, where curiosity meets discovery and every click unveils a world of possibilities. Whether you're a seasoned explorer or a newcomer seeking inspiration, our site is designed with you in mind. Dive into our collection of articles, resources, and insights crafted to ignite your imagination and satisfy your thirst for knowledge. From informative guides to thought-provoking content, embark on a journey where learning knows no bounds. Join us as we traverse the digital realm together, exploring, learning, and growing. Your presence enriches our virtual haven, and we're excited to share this digital adventure with you. Welcome, fellow seeker, to our corner of the web!</h1>
+
+</div>
