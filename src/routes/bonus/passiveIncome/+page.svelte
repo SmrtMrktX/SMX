37c60ebd8c458ee656374888fa72bc04f1cc2e81
d3j1x <d3j1x@yahoo.com>
2024-01-02 14:54:35 +0100

    Add Bonus page

---
+++ src/routes/bonus/passiveIncome/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+    import { Breadcrumb, BreadcrumbItem, Card } from 'flowbite-svelte';
+    import { Button } from 'flowbite-svelte';
+    import { ArrowRightOutline, ShoppingCartSolid } from 'flowbite-svelte-icons';
+    import pwnslg from "$lib/img/pwnslg.png";
+    import honeygainlg from "$lib/img/honeygainlg.png";
+    import traffmonetizerlg from "$lib/img/traffmonetizerlg.png";
+    import packetstreamlg from "$lib/img/packetstreamlg.png"
+</script>
+
+
+<Breadcrumb aria-label="Default breadcrumb example" class="my-4">
+    <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+    <BreadcrumbItem href="/bonus">Bonus</BreadcrumbItem>
+    <BreadcrumbItem>Passive Income</BreadcrumbItem>
+</Breadcrumb>
+
+
+
+  
+  <Card padding="none" size="xl" class="grid md:grid-cols-2 m-auto ">
+    <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
+      <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy money<br>Make Money by Sharing Your Internet</h3>
+        <p class="my-4 font-light">If you care for your time, Pawns.app lets you earn money instantly. All you have to do to start generating income is create a free account and Share Your Internet."</p>
+        <Button color="green">
+            <a href="https://pawns.app/?r=1255808" target="_blank" rel="noreferrer">CLAIM NOW</a><ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
+        </Button>
+    </blockquote>
+      
+      <figcaption class="flex justify-center items-center space-x-3 rtl:space-x-reverse">
+        <img class="w-9 h-9 rounded-full" src={pwnslg} alt="Karen profile" />
+        <div class="space-y-0.5 font-medium dark:text-white text-left">
+          <div>Pawns App</div>
+          <div class="text-sm font-light text-gray-500 dark:text-gray-400">Mobile | Desktop</div>
+        </div>
+      </figcaption>
+    </figure>
+    <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-tr-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy money<br>Make Money by Sharing Your Internet</h3>
+        <p class="my-4 font-light">Honeygain lets you earn passive income by sharing your internet connection! Download the Honeygain app today and make money effortlessly.</p>
+        <Button color="green">
+          <a href="https://r.honeygain.me/CRYPTDED49" target="_blank" rel="noreferrer">CLAIM NOW</a><ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
+      </Button>
+      </blockquote>
+      <figcaption class="flex justify-center items-center space-x-3 rtl:space-x-reverse">
+        <img class="w-9 h-9 rounded-full" src={honeygainlg} alt="Robert profile" />
+        <div class="space-y-0.5 font-medium dark:text-white text-left">
+          <div>Honeygain</div>
+          <div class="text-sm font-light text-gray-500 dark:text-gray-400">Mobile | Desktop</div>
+        </div>
+      </figcaption>
+    </figure>
+    <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-e dark:bg-gray-800 dark:border-gray-700">
+      <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy money<br>Make Money by Sharing Your Internet</h3>
+        <p class="my-4 font-light">TraffMonetizer will help you convert Internet connection on your devices into money. Download it onto your laptop, smartphone, or tablet to start selling your traffic today.</p>
+        <Button color="green">
+          <a href="https://traffmonetizer.com/?aff=1586116" target="_blank" rel="noreferrer">CLAIM NOW</a><ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
+        </Button>
+      </blockquote>
+      <figcaption class="flex justify-center items-center space-x-3 rtl:space-x-reverse">
+        <img class="w-9 h-9 rounded-full" src={traffmonetizerlg} alt="TraffMonetizer Logo" />
+        <div class="space-y-0.5 font-medium dark:text-white text-left">
+          <div>TRAFFMONETIZER</div>
+          <div class="text-sm font-light text-gray-500 dark:text-gray-400">Mobile | Desktop</div>
+        </div>
+      </figcaption>
+    </figure>
+    <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-e dark:bg-gray-800 dark:border-gray-700">
+      <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy money<br>Make Money by Sharing Your Internet</h3>
+        <p class="my-4 font-light">PacketStream is a Residential Proxy Network powered by real residential IPs and ... Legit app for passive earning by sharing bandwidth, pays for sure, earns ...</p>
+        <Button color="green">
+          <a href="https://packetstream.io/?psr=5zci" target="_blank" rel="noreferrer">CLAIM NOW</a><ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
+        </Button>
+      </blockquote>
+      <figcaption class="flex justify-center items-center space-x-3 rtl:space-x-reverse">
+        <img class="w-9 h-9 rounded-full" src={packetstreamlg} alt="TraffMonetizer Logo" />
+        <div class="space-y-0.5 font-medium dark:text-white text-left">
+          <div>PacketStream</div>
+          <div class="text-sm font-light text-gray-500 dark:text-gray-400">Desktop</div>
+        </div>
+      </figcaption>
+    </figure>
+
+  </Card>
+
+
