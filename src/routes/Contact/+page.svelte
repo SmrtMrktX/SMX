7b133e202f9a44ed6e29bd1cec52db18306f9cb9
d3j1x <d3j1x@yahoo.com>
2024-01-04 01:42:20 +0100

    add social link

--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -15,14 +15,16 @@
 
   
 
-<div class="justify-center items-center text-center text-4xl m-20">
+<div class="justify-center items-center text-center text-4xl my-20">
     <h1 class="text-6xl font-semibold text-purple-500">Social</h1>
     
-    <div class="grid grid-cols-2 gap-10 md:grid-cols-4 md:gap-20 justify-center items-center text-center space-x-4 my-20">
+    <div class="grid grid-cols-2 gap-10 md:grid-cols-3 md:gap-20 justify-center items-center text-center space-x-4 my-20 mx-10">
     <a href="https://www.facebook.com/smrtmrktx" target="_blank" class="text-black dark:text-white hover:text-blue-500"><i class="fab fa-facebook text-6xl md:text-8xl"></i></a>
     <a href="https://twitter.com/SmrtMrket" target="_blank" class="text-black dark:text-white hover:text-blue-400"><i class="fab fa-twitter text-6xl md:text-8xl"></i></a>
     <a href="https://www.linkedin.com/in/smrtmrktx/" target="_blank" class="text-black dark:text-white hover:text-blue-700"><i class="fab fa-linkedin text-6xl md:text-8xl"></i></a>
     <a href="https://www.github.com/SmrtMrktX" target="_blank" class="text-black dark:text-white hover:text-gray-700"><i class="fab fa-github text-6xl md:text-8xl"></i></a>
+    <a href="https://www.tiktok.com/@smrtmrktx" target="_blank" class="text-black dark:text-white hover:text-pink-500"><i class="fab fa-tiktok text-6xl md:text-8xl"></i></a>
+    <a href="https://www.instagram.com/smrtmrket/" target="_blank" class="text-black dark:text-white hover:text-pink-500"><i class="fab fa-instagram text-6xl md:text-8xl"></i></a>
     </div>
     
     <h1 class="text-6xl font-semibold text-purple-500">Mail</h1>
