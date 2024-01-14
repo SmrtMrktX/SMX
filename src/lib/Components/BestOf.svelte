b9bbe47a2eb2e6889ec3884921b064ce57346b09
d3j1x <d3j1x@yahoo.com>
2024-01-14 23:18:51 +0100

    theme

--- src/lib/Components/BestOf.svelte
+++ src/lib/Components/BestOf.svelte
@@ -1,6 +1,6 @@
 
 <button style="font-family: 'Russo One', sans-serif;"
-class=" text-purple-600 bg-black dark:bg-white  dark:shadow-black justify-center items-center text-center   py-1 my-52 max-w-4xl lg:max-w-6xl mx-auto border-8 rounded-xl shadow-xl dark:hover:bg-black hover:bg-white hover:uppercase">
+class=" text-purple-600    dark:shadow-black justify-center items-center text-center   py-1 my-52 max-w-4xl lg:max-w-6xl mx-auto border-8 rounded-xl shadow-xl hover:bg-black dark:hover:bg-white hover:uppercase">
   
     <span class="p-0 text-center text-3xl md:text-4xl lg:text-5xl ">
         <a href="/bestOf">
