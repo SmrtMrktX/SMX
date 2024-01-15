1238763c34f441c0568127d68ab5222473928fee
d3j1x <d3j1x@yahoo.com>
2024-01-15 17:17:13 +0100

    component and design update

--- src/routes/ULTIMATE/+page.svelte
+++ src/routes/ULTIMATE/+page.svelte
@@ -3,9 +3,7 @@
     import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
     import ComingSoon from "$lib/Components/ComingSoon.svelte";
     import SmxLoader from "$lib/Components/SmxLoader.svelte";
-  
-    import webappslg from "$lib/img/products/webapps.webp";
-  
+    
   
     let x =true;
     const ToOther = () => {
@@ -26,7 +24,7 @@
   
   <Breadcrumb aria-label="Default breadcrumb example" class="my-4">
       <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
-      <BreadcrumbItem>BEST OF</BreadcrumbItem>
+      <BreadcrumbItem>ULTIMATE</BreadcrumbItem>
   </Breadcrumb>
   
   
