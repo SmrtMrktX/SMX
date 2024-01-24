5a62548e2d90f9759f4b58e7655295a71ab743d4
d3j1x <d3j1x@yahoo.com>
2024-01-24 17:26:31 +0100

    Add Arabic web apps

--- src/routes/products/webApps/+page.svelte
+++ src/routes/products/webApps/+page.svelte
@@ -5,6 +5,7 @@
   
     import SmxLoader from "$lib/Components/SmxLoader.svelte";
 
+    import arabicwebappslg from "$lib/img/products/webApps/arabicwebappslg.webp";
     import crudapplg from "$lib/img/products/webApps/crudapp.webp";
     import basicwebpagelg from "$lib/img/products/webApps/basicwebpage.webp";
     import blackjacklg from "$lib/img/products/webApps/blackjackwebapp.webp";
@@ -48,6 +49,32 @@
 
 
   <div class="gap-8 grid  md:grid-cols-2 md:gap-12 lg:grid-cols-3 lg:gap-16 m-auto py-10">
+
+    <div  class="space-y-4 m-auto w-72 max-h-xs">
+          
+      <Card img={arabicwebappslg}>
+          
+        <div class="justify-end items-end text-end self-end">
+            <Badge border color="green" >Free</Badge>
+        </div> 
+          
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Arabic Web Apps.</h5>
+          
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">HTML, CSS, JS.</p>
+        
+
+        <div class="mx-auto mb-3 text-center rounded border-2">
+          <a style="font-family: 'Russo One', sans-serif;" href="https://github.com/smrket/Arabic-Webapps" target="_blank" class="text-purple-600 mx-2">Source code</a>
+        </div>
+        
+        <Button href="https://smrket.github.io/Arabic-Webapps/" target="_blank">
+          Live App<ArrowRightOutline class="w-3.5 h-3.5 ms-2 text-white" />
+        </Button>
+
+        </Card>
+    </div>
+
+
 
     <div  class="space-y-4 m-auto w-72 max-h-xs">
           
