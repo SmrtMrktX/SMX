28104161cfad0e8e2f3b1803a498ce7606e38d44
d3j1x <d3j1x@yahoo.com>
2025-03-15 18:01:51 +0100

    button update

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -2,10 +2,8 @@
 	<div class="hero-content text-center">
 		<div class="max-w-md">
 			<h1 class="text-5xl font-bold">Products</h1>
-			<a href="/">
-				<button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl my-10"
-					><i class="fas fa-long-arrow-alt-left mr-2"></i>Back
-				</button>
+			<a href="/" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl my-10">
+				<i class="fas fa-long-arrow-alt-left mr-2"></i>Back
 			</a>
 		</div>
 	</div>
