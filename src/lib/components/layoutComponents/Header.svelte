178cb2527991b07694adc3ab1cd36b351a858558
d3j1x <d3j1x@yahoo.com>
2025-03-16 03:39:42 +0100

    add version dropdown button

--- src/lib/components/layoutComponents/Header.svelte
+++ src/lib/components/layoutComponents/Header.svelte
@@ -20,7 +20,6 @@
 				<li><a class={mynavlink} href="/">Homepage</a></li>
 				<li><a class={mynavlink} href="/products">Products</a></li>
 				<li><a class={mynavlink} href="/about">About</a></li>
-				<li class="text-center font-mono text-xs">version 0.0.4</li>
 			</ul>
 		</div>
 	</div>
@@ -28,6 +27,13 @@
 		<a href="/" class="btn btn-ghost text-2xl tracking-widest md:text-4xl">SMRT MRKT X </a>
 	</div>
 	<div class="navbar-end">
+		<div class="dropdown dropdown-start mr-4 text-sm">
+			<div tabindex="0" role="button" class="btn m-1">V.1 beta</div>
+			<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 ml-1 p-2 shadow-sm">
+				<li><a href="/">V.1</a></li>
+				<li><a href="https://v0.smx.tn" target="_blank">V.0</a></li>
+			</ul>
+		</div>
 		<input type="checkbox" value="dark" class="toggle theme-controller" />
 	</div>
 </div>
