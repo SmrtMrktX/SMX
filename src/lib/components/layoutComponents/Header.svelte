81980c1e252acf3eb180d336c5bb8b4a33e8b23a
d3j1x <d3j1x@yahoo.com>
2025-03-26 23:13:04 +0100

    logo updated

--- src/lib/components/layoutComponents/Header.svelte
+++ src/lib/components/layoutComponents/Header.svelte
@@ -3,11 +3,11 @@
 	let mynavlink = 'text-sm';
 </script>
 
-<div class="navbar bg-base-100 sticky top-0 z-50 font-extrabold shadow-sm">
+<div class="navbar bg-base-100 shadow-base-200 sticky top-0 z-50 font-extrabold shadow-sm">
 	<div class="navbar-start">
-		<div class="dropdown ml-2">
-			<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-				<img class="scale-125 border-b-4 border-green-500" src={smxLogo} alt="smx logo" />
+		<div class="dropdown z-50 ml-2 p-2">
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle w-14">
+				<img class=" rounded-full" src={smxLogo} alt="smx logo" />
 			</div>
 			<ul class="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow">
 				<li><a class={mynavlink} href="/">Home</a></li>
@@ -17,11 +17,13 @@
 			</ul>
 		</div>
 	</div>
-	<div class="navbar-center">
-		<a href="/" class="btn btn-ghost text-2xl font-extrabold md:text-4xl">SMRT MRKT X </a>
+	<div class="fixed top-4 left-1/2 w-full -translate-x-1/2 transform text-center">
+		<a href="/" class="btn btn-ghost text-2xl font-extrabold tracking-widest md:text-4xl"
+			>SMRT MRKT X
+		</a>
 	</div>
 	<div class="navbar-end">
-		<div class="dropdown dropdown-start mr-6 text-sm font-extrabold">
+		<div class="dropdown dropdown-start mr-4 text-sm font-extrabold">
 			<div tabindex="0" role="button" class="btn rounded-sm font-extrabold text-green-500">V.1</div>
 			<ul class="dropdown-content menu bg-base-100 rounded-box z-1 p-2 shadow-sm">
 				<li><a href="/">V.1</a></li>
